d50ccd8a4dea0ae6880e98aa3cf59965bf264d1b
Abel <abel.ronin@gmail.com>
2021-11-06 23:17:04 +0100

    Use of commons-alumnos dependency

--- src/main/java/com/curso/microservicios/app/usuarios/controllers/AlumnoController.java
+++ src/main/java/com/curso/microservicios/app/usuarios/controllers/AlumnoController.java
@@ -1,12 +1,14 @@
 package com.curso.microservicios.app.usuarios.controllers;
 
-import com.curso.microservicios.app.usuarios.models.entity.Alumno;
 import com.curso.microservicios.app.usuarios.services.AlumnoService;
+import com.curso.microservicios.commons.alumnos.models.entity.Alumno;
 import com.curso.microservicios.commons.controllers.CommonController;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
