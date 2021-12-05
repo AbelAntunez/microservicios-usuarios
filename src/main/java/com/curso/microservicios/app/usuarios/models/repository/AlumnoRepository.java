dbe822dad902468febcc1fb8f7c40574ff83a2ec
Abel Antunez de la Fuente <abel.antunez@tink.com>
2021-12-05 19:27:40 +0100

    [FIX] Changed java version and added Alumno dependency

--- src/main/java/com/curso/microservicios/app/usuarios/models/repository/AlumnoRepository.java
+++ src/main/java/com/curso/microservicios/app/usuarios/models/repository/AlumnoRepository.java
@@ -1,6 +1,6 @@
 package com.curso.microservicios.app.usuarios.models.repository;
 
-import com.curso.microservicios.app.usuarios.models.entity.Alumno;
+import com.curso.microservicios.commons.alumnos.models.entity.Alumno;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AlumnoRepository extends CrudRepository<Alumno, Long> {
--- src/main/java/com/curso/microservicios/app/usuarios/services/AlumnoServiceImpl.java
+++ src/main/java/com/curso/microservicios/app/usuarios/services/AlumnoServiceImpl.java
@@ -1,7 +1,7 @@
 package com.curso.microservicios.app.usuarios.services;
 
-import com.curso.microservicios.app.usuarios.models.entity.Alumno;
 import com.curso.microservicios.app.usuarios.models.repository.AlumnoRepository;
+import com.curso.microservicios.commons.alumnos.models.entity.Alumno;
 import com.curso.microservicios.commons.services.CommonServiceImpl;
 import org.springframework.stereotype.Service;
 
